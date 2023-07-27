4b26bd7157bc46bb680b49b852f9495180955ed6
Dong Woo Lee <don04lee@gmail.com>
2023-07-26 19:58:08 -0700

    Complete calculator project
    Implemented scroll bar in display to combat overflow.
    
    Rewrote most of logic in index.js to make it work with consecutive
    operations that do not use the equal sign.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Odin Calculator
 
-This is my student solution to The Odin Project's curriculum of the calculator.
+This is my student solution to The Odin Project's curriculum of the calculator. This program attempts to replicate the functions of a calculator. This project incorporated the languages of HTML, JS, and CSS, with the project hoping to follow the logic of a normal calculator. 
+
+The main difficulty that was found was the logic behind the calculator itself, with keeping track of the various possibilities of choosing operators and operands and how that would affect the type of operation being done. In addition, a major difficulty was identifying how to combat the problem of overflowing numbers, which I solved by deciding to use a scroll bar within the display of the calculator. However, it took numerous attempts to realize that the display of flexbox was incompatible with the scroll bar.
+
+In the future, I would like to try and add additional functions that would make this calculator much more advanced similar to that of a scientific calculator for now. I do also have an idea for a video game that incorporates the framework of a calculator, and so I will be attempting to do that in the future.
+
+![Calculator Image](https://github.com/don04lee/odin-calculator/blob/main/calculator.png)
--- index.js
+++ index.js
@@ -1,10 +1,118 @@
 // creating base of an operation
 let firstNum;
 let operator;
-let secondNum;
+let previousKey;
+
+let start = true;
+
+// establish display
+let display = document.querySelector('.display');
+
+// establishing buttons
+let allNums = document.querySelectorAll('.number');
+let clearButton = document.querySelector('#ac');
+let operations = document.querySelectorAll('.operation');
+let equals = document.querySelector('#equals');
+let deletes = document.querySelector('#delete');
+let decimal = document.querySelector('#decimal');
+let sign = document.querySelector('#sign');
+let percent = document.querySelector('#percent');
+let buttons = document.getElementById('buttons').children;
+
+for(let button of buttons) {
+  button.addEventListener('click', function() {
+    let curr = button;
+    for(let b of buttons) {
+      if(b !== curr) {
+        b.classList.remove('active');
+      }
+    }
+    button.classList.toggle('active');
+  });
+};
+
+allNums.forEach(num => {
+  num.addEventListener('click', function() {
+    if(display.textContent == "0" || previousKey == "operator") {
+      display.textContent = num.textContent;
+    }
+    else {
+      display.textContent += "" + num.textContent;
+    }
+    previousKey = "num";
+  });
+}); 
+
+operations.forEach(op => {
+  op.addEventListener('click', function() {
+    let curr = op;
+    console.log(firstNum + " " + display.textContent);
+    if(previousKey == "num" && firstNum == null) {
+      firstNum = display.textContent;
+    }
+    else if(previousKey == "num") {
+      firstNum = operate(operator, firstNum, display.textContent);
+      display.textContent = firstNum;
+    }
+    operator = op.textContent;
+    previousKey = "operator";
+  });
+});
+
+clearButton.addEventListener('click', function() {
+  reset();
+});
+
+equals.addEventListener('click', function() {
+  if(previousKey == "num" && firstNum != null) {
+    firstNum = operate(operator, firstNum, display.textContent);
+    display.textContent = firstNum;
+  }
+  previousKey = "operator";
+});
+
+decimal.addEventListener('click', function() {
+  if(display.textContent != "0" && !display.textContent.includes(".")) {
+    display.textContent += ".";
+    previousKey = "num";
+  }
+});
+
+deletes.addEventListener('click', function() {
+  if(display.textContent != "0") {
+    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+  }
+
+  if(display.textContent == "") {
+    display.textContent = "0";
+  }
+  previousKey = "num";
+});
+
+sign.addEventListener('click', function() {
+  if(display.textContent != "0") {
+    display.textContent *= -1;
+    firstNum = display.textContent;
+    previousKey = "operator";
+  }
+})
+
+percent.addEventListener('click', function() {
+  if(display.textContent != "0") {
+    display.textContent /= 100;
+    firstNum = display.textContent;
+    previousKey = "operator";
+  }
+})
+
+function reset() {
+  firstNum = null;
+  operator = null;
+  display.textContent = "0";
+}
 
 function add(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 }
 
 function multiply(a, b) {
